dfa28b875a1c5a0ef19eaee92312508ff97b7e48
Joe Sutton <hungryed6@gmail.com>
2014-02-06 22:46:13 -0500

    added tests to toggler

---
+++ app/assets/javascripts/toggler.js
@@ -0,0 +1,17 @@
+function init() {
+  var $toggle_links = $(".detail_toggle")
+  $toggle_links.click(function(event){
+    var $detail = $(this).siblings(".detail");
+    if ($detail.hasClass("hidden")) {
+      $(this).text("Hide Details");
+    } else {
+      $(this).text("Show Details");
+    }
+    $detail.toggleClass("hidden");
+    event.preventDefault();
+  });
+}
+
+$(function() {
+  init();
+})
